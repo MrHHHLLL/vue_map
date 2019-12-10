c220fcb68b2fe5255beed6f7bca17ae0c4e24862
MrHHHLLL <49604794+MrHHHLLL@users.noreply.github.com>
2019-12-10 11:19:31 +0800

    a

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import App from './App.vue';
+import BaiduMap from 'vue-baidu-map';
+
+Vue.use(BaiduMap, {
+  // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+  ak: 'KwbvZK7yUSxkdaE6wqn2Gs73GKoGtejs'
+});
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app');
